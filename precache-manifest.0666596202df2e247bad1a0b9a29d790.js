f1d63d603aeb1e05270ab63945f5231ffa7e9a82
Ling Chen <duke@impactnorth.com>
2020-12-01 09:54:57 -0500

    Updates

---
+++ precache-manifest.0666596202df2e247bad1a0b9a29d790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e048a075998d22acb112a15f9a0cc7a",
+    "url": "/proshop/index.html"
+  },
+  {
+    "revision": "c8317974bbde77d21fd1",
+    "url": "/proshop/static/css/main.010fcd9a.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/proshop/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/proshop/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8317974bbde77d21fd1",
+    "url": "/proshop/static/js/main.1b541d71.chunk.js"
+  },
+  {
+    "revision": "fb0d10e6f54c47b5cc10",
+    "url": "/proshop/static/js/runtime-main.08c65974.js"
+  }
+]);
